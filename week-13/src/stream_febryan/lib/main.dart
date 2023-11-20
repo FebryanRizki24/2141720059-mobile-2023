a62755be404ed79c993ff87b4918ab13690ed766
Febryan Rizki <febryanrizki24@gmail.com>
2023-11-20 23:01:24 +0700

    W13: Jawaban Soal 7

--- week-13/src/stream_febryan/lib/main.dart
+++ week-13/src/stream_febryan/lib/main.dart
@@ -56,6 +56,10 @@ class _StreamHomePageState extends State<StreamHomePage> {
       setState(() {
         lastNumber = event;
       });
+    }).onError((error) {
+      setState(() {
+        lastNumber = -1;
+      });
     });
     super.initState();
     // colorStream = ColorStream();
@@ -72,6 +76,7 @@ class _StreamHomePageState extends State<StreamHomePage> {
     Random random = Random();
     int myNum = random.nextInt(10);
     numberStream.addNumberToSink(myNum);
+    // numberStream.addError();
   }
 
   @override
