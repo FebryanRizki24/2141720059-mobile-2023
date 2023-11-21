81beb7b361b6f11acd56c0b9583ff95308c8a17a
Febryan Rizki <febryanrizki24@gmail.com>
2023-11-21 10:22:06 +0700

    W13: Jawaban Soal 8

--- week-13/src/stream_febryan/lib/main.dart
+++ week-13/src/stream_febryan/lib/main.dart
@@ -32,6 +32,7 @@ class _StreamHomePageState extends State<StreamHomePage> {
   int lastNumber = 0;
   late StreamController numberStreamController;
   late NumberStream numberStream;
+  late StreamTransformer transformer;
 
   void changeColor() async {
     // await for (var eventColor in colorStream.getColors()) {
@@ -52,7 +53,17 @@ class _StreamHomePageState extends State<StreamHomePage> {
     numberStream = NumberStream();
     numberStreamController = numberStream.controller;
     Stream stream = numberStreamController.stream;
-    stream.listen((event) {
+    // stream.listen((event) {
+    //   setState(() {
+    //     lastNumber = event;
+    //   });
+    // }).onError((error) {
+    //   setState(() {
+    //     lastNumber = -1;
+    //   });
+    // });
+
+    stream.transform(transformer).listen((event) {
       setState(() {
         lastNumber = event;
       });
@@ -61,6 +72,16 @@ class _StreamHomePageState extends State<StreamHomePage> {
         lastNumber = -1;
       });
     });
+
+    transformer = StreamTransformer<int, int>.fromHandlers(
+        handleData: (value, sink) {
+          sink.add(value * 10);
+        },
+        handleError: (error, trace, sink) {
+          sink.add(-1);
+        },
+        handleDone: (sink) => sink.close());
+
     super.initState();
     // colorStream = ColorStream();
     // changeColor();
